70bf69ad3de8f5817b7f28670bfcea05e3a8dac4
Arnold Ngassam <ngassam.arnold@nanobnk.com>
2022-12-15 11:58:34 +0400

    Add UI cartArticle

--- app/src/main/java/com/app/trainning/MainActivity.kt
+++ app/src/main/java/com/app/trainning/MainActivity.kt
@@ -3,13 +3,22 @@ package com.app.trainning
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.app.trainning.ui.cardArticle.AticleCard
 import com.app.trainning.ui.theme.TestComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +31,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    AppCard()
                 }
             }
         }
@@ -30,14 +39,20 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun AppCard(){
+    val image= painterResource(R.drawable.bg_compose_background);
+    AticleCard(
+        image = image,
+        title = stringResource(R.string.title),
+        description = stringResource(R.string.description),
+        content = stringResource(R.string.content)
+    )
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun DefaultPreview() {
     TestComposeTheme {
-        Greeting("Android")
+        AppCard()
     }
 }
