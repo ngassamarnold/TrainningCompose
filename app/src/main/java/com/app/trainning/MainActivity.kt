d93aee530af3694dcdd2253bdded84a47f9a91cd
Arnold Ngassam <ngassam.arnold@nanobnk.com>
2022-12-16 20:42:18 +0400

    Adding Dice Roller

--- app/src/main/java/com/app/trainning/MainActivity.kt
+++ app/src/main/java/com/app/trainning/MainActivity.kt
@@ -1,17 +1,21 @@
 package com.app.trainning
 
-import CardBusiness
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.runtime.Composable
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.app.trainning.ui.theme.TestComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,31 +28,48 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    AppCardBusiness()
+                    DiceRollerApp()
                 }
             }
         }
     }
 }
 
+@Composable
+fun DiceRollerApp(){
+    DiceWithButtonImage(modifier= Modifier
+        .fillMaxSize()
+        .wrapContentSize(Alignment.Center)
+    )
+}
 
 @Composable
-fun AppCardBusiness(){
-    val image= painterResource(R.drawable.android_logo)
-    CardBusiness(
-        name = stringResource(R.string.name),
-        position = stringResource(R.string.position),
-        picture = image,
-        phone = stringResource(R.string.phone),
-        email = stringResource(R.string.email),
-        share = stringResource(R.string.share)
-    )
+fun DiceWithButtonImage(modifier:Modifier= Modifier) {
+    var result by remember { mutableStateOf( 1) }
+    val image = when(result){
+        1-> R.drawable.dice_1
+        2-> R.drawable.dice_2
+        3-> R.drawable.dice_3
+        4-> R.drawable.dice_4
+        5-> R.drawable.dice_5
+        else -> R.drawable.dice_6
+    }
+    Column(
+        modifier=modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(painter = painterResource(image) , contentDescription = result.toString())
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = { result = (1..6).random()}) {
+            Text(text = stringResource(R.string.roll))
+        }
+    }
 }
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun DefaultPreview() {
     TestComposeTheme {
-        AppCardBusiness()
+        DiceRollerApp()
     }
 }
