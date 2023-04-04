2f920a3ab2806782c04ffadd66a6377dc812aae8
Arnold Ngassam <ngassam.arnold@nanobnk.com>
2023-04-04 19:23:27 +0400

    Calculate Tip

--- app/src/main/java/com/app/trainning/ui/input/TextField.kt
+++ app/src/main/java/com/app/trainning/ui/input/TextField.kt
@@ -1,12 +1,16 @@
 package com.app.trainning.ui.input
 
+import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.Placeholder
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import com.app.trainning.R
 
@@ -14,14 +18,18 @@ import com.app.trainning.R
 fun EditNumberField(
     value: String,
     onValueChanged: (value: String) -> Unit,
+    @StringRes placeholder: Int = R.string.cost_of_service,
+    keyboardOptions: KeyboardOptions,
+    keyboardActions: KeyboardActions
 ) {
 
     TextField(
         label = {
-            Text(text = stringResource(R.string.cost_of_service))
+            Text(text = stringResource(id = placeholder))
         },
         singleLine = true,
-        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+        keyboardOptions = keyboardOptions,
+        keyboardActions=keyboardActions,
         modifier = Modifier.fillMaxWidth(),
         value = value,
         onValueChange = onValueChanged
--- app/src/main/java/com/app/trainning/ui/tipTime/TipTime.kt
+++ app/src/main/java/com/app/trainning/ui/tipTime/TipTime.kt
@@ -1,29 +1,41 @@
 package com.app.trainning.ui.tipTime
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Switch
+import androidx.compose.material.SwitchDefaults
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.motionEventSpy
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.app.trainning.R
 import com.app.trainning.ui.input.EditNumberField
 import java.text.NumberFormat
 
 @Composable
-fun TipTimeScreen(text1:Int, text2: Int) {
+fun TipTimeScreen(text1: Int, text2: Int) {
     var amountInput by remember { mutableStateOf("") }
     val amount = amountInput.toDoubleOrNull() ?: 0.0
-    val tip = calculateTip(amount)
+    var tipInput by remember { mutableStateOf("") }
+    val tipPercent = tipInput.toDoubleOrNull() ?: 0.0
+    var roundUp by remember { mutableStateOf(false) }
+    val tip = calculateTip(amount, tipPercent, roundUp)
 
-    val onValueChanged: (value: String) -> Unit = {
-        amountInput = it
-    }
-
+    val focusManager = LocalFocusManager.current
     Column(
-        modifier = Modifier.padding(32.dp),
+        modifier = Modifier.padding(20.dp),
         verticalArrangement = Arrangement.spacedBy(8.dp),
     ) {
         Text(
@@ -32,7 +44,30 @@ fun TipTimeScreen(text1:Int, text2: Int) {
             modifier = Modifier.align(Alignment.CenterHorizontally)
         )
         Spacer(modifier = Modifier.height(16.dp))
-        EditNumberField(value = amountInput, onValueChanged = onValueChanged)
+        EditNumberField(
+            value = amountInput,
+            onValueChanged = { amountInput = it },
+            keyboardOptions = KeyboardOptions.Default.copy(
+                imeAction = ImeAction.Next,
+                keyboardType = KeyboardType.Number
+            ),
+            keyboardActions = KeyboardActions(
+                onNext = { focusManager.moveFocus(FocusDirection.Down) }
+            )
+        )
+        EditNumberField(
+            value = tipInput,
+            onValueChanged = { tipInput = it },
+            placeholder = R.string.tip,
+            keyboardOptions = KeyboardOptions.Default.copy(
+                imeAction = ImeAction.Done,
+                keyboardType = KeyboardType.Number
+            ),
+            keyboardActions = KeyboardActions(
+                onDone = { focusManager.clearFocus() }
+            )
+        )
+        RounndtheTipRow(text2, tip, roundUp, onRoundChange = { roundUp = it })
         Spacer(modifier = Modifier.height(24.dp))
         Text(
             text = stringResource(text2, tip),
@@ -43,9 +78,43 @@ fun TipTimeScreen(text1:Int, text2: Int) {
     }
 }
 
+@Composable
+fun RounndtheTipRow(
+    text2: Int,
+    tip: String,
+    roundUp: Boolean,
+    onRoundChange: (value: Boolean) -> Unit
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .size(48.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = stringResource(text2, tip),
+            fontSize = 12.sp,
+        )
+        Switch(
+            checked = roundUp,
+            onCheckedChange = onRoundChange,
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentWidth(Alignment.End),
+            colors = SwitchDefaults.colors(uncheckedThumbColor = Color.DarkGray)
+
+        )
+    }
+}
+
 private fun calculateTip(
-    amount: Double, tipPercent: Double = 15.0
+    amount: Double,
+    tipPercent: Double = 15.0,
+    roundUp: Boolean
 ): String {
-    val tip = tipPercent / 100 * amount
+    var tip = tipPercent / 100 * amount
+    if (roundUp)
+        kotlin.math.ceil(tip)
+
     return NumberFormat.getCurrencyInstance().format(tip)
 }
